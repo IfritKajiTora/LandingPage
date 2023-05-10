378e68a15dd16e263996d8f3dd6f7ec6c1b5c09a
IfritKajiTora <129754900+IfritKajiTora@users.noreply.github.com>
2023-05-10 20:52:29 +0200

    updated React/Next Project

--- src/constants/portfolio/reactNext.tsx
+++ src/constants/portfolio/reactNext.tsx
@@ -2,6 +2,13 @@ import {Project} from '@/constants/portfolio/types/types'
 
 const ReactNext:Project[] = [{
   id: 1,
+  bgImage: '/img/portfolio/LandingPagePortfolio.jpg',
+  link: 'https://landing-page-xi-indol.vercel.app/',
+  title: 'Landing Page Portfolio',
+  info: 'React\nNext\nTypeScript\nTailwind',
+  target: '_blank'
+  },{
+  id: 2,
   bgImage: '/img/portfolio/questionPage.jpg',
   link: '/not-yet-made',
   title: 'W trakcie tworzenia',
