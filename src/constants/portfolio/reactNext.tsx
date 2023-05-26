87337c4ed8245ecd797c8e0d5297e486a4b85710
IfritKajiTora <129754900+IfritKajiTora@users.noreply.github.com>
2023-05-27 01:24:51 +0200

    Added New Project to Portfolio

--- src/constants/portfolio/reactNext.tsx
+++ src/constants/portfolio/reactNext.tsx
@@ -1,20 +1,29 @@
 import {Project} from '@/constants/portfolio/types/types'
 
-const ReactNext:Project[] = [{
-  id: 1,
-  bgImage: '/img/portfolio/LandingPagePortfolio.jpg',
-  link: 'https://landing-page-xi-indol.vercel.app/',
-  title: 'Landing Page Portfolio',
-  info: 'React\nNext\nTypeScript\nTailwind',
-  target: '_blank'
-  },{
+const ReactNext:Project[] = [
+  {
+    id: 1,
+    bgImage: '/img/portfolio/LandingPagePortfolio.jpg',
+    link: 'https://landing-page-xi-indol.vercel.app/',
+    title: 'Landing Page Portfolio',
+    info: 'React\nNext\nTypeScript\nTailwind',
+    target: '_blank'
+    },{
+      id: 2,
+      bgImage: '/img/portfolio/pogoda-trojmiasta.jpg',
+      link: 'https://pogoda-trojmiasto.vercel.app/',
+      title: 'Pogoda Trójmiasta',
+      info: 'React\nNext\nTypeScirpt\nTailwind',
+      target: '_blank'
+    },
+  /*{
   id: 2,
   bgImage: '/img/portfolio/questionPage.jpg',
   link: '/not-yet-made',
   title: 'W trakcie tworzenia',
   info: 'React\nNext\nTypeScript\nTailwind',
   target: '_self'
-  }
+  }*/
 ]
 
 export default ReactNext;
